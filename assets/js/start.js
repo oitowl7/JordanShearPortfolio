1e5a233a79f87fe2f47080af93020e5fd4dc27e8
unknown <shear_jbootcamp@hotmail.com>
2018-01-23 17:32:36 -0500

    initial push

---
+++ assets/js/start.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDTek1jmISxA97XiCrsuIMYqP4hwtwxQkw",
+    authDomain: "portfolio-ce756.firebaseapp.com",
+    databaseURL: "https://portfolio-ce756.firebaseio.com",
+    projectId: "portfolio-ce756",
+    storageBucket: "",
+    messagingSenderId: "483015153440"
+  };
+  firebase.initializeApp(config);
